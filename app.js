00bba924d399808b52f49ca501fb1211509c92bf
deborahebell <64565513+deborahebell@users.noreply.github.com>
2020-07-23 17:50:57 -0400

    [Construction] making a fucntion

--- app.js
+++ app.js
@@ -39,3 +39,37 @@ console.log(designer);
 console.log(designerTwo);
 console.log(designerTwo.desType);
 console.log(designerTwo.color);
+
+//Constructor Functions
+
+let stacy = new user('stacy')
+let lizzie = new user('lizzie')
+
+//new-user is optional name for the function
+
+console.log(stacy.name);
+console.log(izzie.name);
+
+function bodyParts(name, action, location) {
+    this.name = name;
+    this.action = action;
+    this.location = location;
+
+    //return this
+};
+
+let ankles = new bodyParts('tibia', 'walk','distal');
+console.log(ankles);
+console.log(ankles.name);
+
+let arms = new bodyParts('ulna','lift', 'proximal');
+let torso = new bodyParts('abdomen','stabilize','medial')
+
+console.log(arms)
+console.log(arms.name)
+
+console.log(torso)
+console.log(torso.name)
+
+// make a constructor function
+// make 3 new variables with that constructor function
